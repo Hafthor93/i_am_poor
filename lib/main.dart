d2f927b29ef6a860170bc96bbb9dc297570a3ee9
Hafthor93 <hafthor93@gmail.com>
2022-03-26 14:02:22 +0000

    Finished

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,52 @@
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'dart:core';
 
-// flutter run --debug
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
-          child: Image.asset('images/pickaxe.png'),
-
-          backgroundColor: Colors.grey,
-        ),
-        backgroundColor: Colors.lightBlueAccent,
-        appBar: AppBar(
-          backgroundColor: Colors.grey,
-          title: Center(
-              child: Text("Press the pickaxe to mine the diamond!",
-                  style: TextStyle(color: Colors.white))),
-        ),
-        body: Center(
-          child: Image.asset('images/mcore.png'),
-        ),
-      ),
-    ),
-  );
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.grey),
+        home: HomeScreen()
+    );
+  }
 }
+  class HomeScreen extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        bottomNavigationBar: Image.asset('images/mcore.png'),
+        backgroundColor: Colors.grey,
+        appBar: AppBar(title: Text('Press the pickaxe to mine!')),
+        body: FloatingActionButton(
+          child: Image.asset('images/pickaxe.png'),
+          onPressed: () {
+            _navigateToNextScreen(context);
+          },
+        ),
+      );
+    }
+  }
+
+  void _navigateToNextScreen(BuildContext context) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => NewScreen()));
+  }
 
 
+class NewScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightBlue,
+      appBar: AppBar(title: Text('Diamond!')),
+      body: Center(
+        child: Image.asset('images/diamondmc.png'),
+      ),
+    );
+  }
+}
